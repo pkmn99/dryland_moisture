e708c196545674c022dc712ddb6fa3afa29bea0f
Yan_dellwork <pkmn99@gmail.com>
2024-01-30 09:23:07 +0800

    add significance mask

--- code/plot_prec_e_corr_map.py
+++ code/plot_prec_e_corr_map.py
@@ -10,16 +10,20 @@ from plot_china_dryland_source import set_lat_lon
 from process_et2prec import load_era5_data,load_gleam_data,load_e2p_data,make_cn_mask
 from plot_pe_prec_corr_map import xr_detrend,xr_stats_corr
 
-def make_plot():
+def make_plot(et_data='GLEAM'):
     #Load data
-    de_ycn_gleam = load_gleam_data('y',cn_label=True) # GLEAM ET China
+    if et_data=='GLEAM':
+        de_ycn_gleam = load_gleam_data('y',cn_label=True) # GLEAM ET China
+    else:
+        de_ycn_gleam = load_era5_data('ET','y',cn_label=True) # ERA5 ET China
+
     dp_ycn=load_era5_data('prec','y',cn_label=True)
 
     cn_mask =make_cn_mask()
     
     # corr between prec con and downwind prec 
-    [r_e_p,p_map]=xr_stats_corr(xr_detrend(dp_ycn.sel(time=slice('2001','2020'))),
-                   xr_detrend(de_ycn_gleam.sel(time=slice('2001','2020'))), dim='time')
+    [r_e_p,p_map]=xr_stats_corr(xr_detrend(dp_ycn.where(cn_mask>0).sel(time=slice('2001','2020')).fillna(0)),
+                   xr_detrend(de_ycn_gleam.where(cn_mask>0).sel(time=slice('2001','2020')).fillna(0)), dim='time')
 
     pr=ccrs.PlateCarree()
 
@@ -27,9 +31,12 @@ def make_plot():
     fig = plt.figure(figsize=[8, 6])
     # panel a,b 
     ax1 = fig.add_axes([0, 0, 1, 1], projection=pr)
-    im = plot_map(r_e_p.where(cn_mask>0),ax=ax1, levels=np.arange(-1,1.01,0.1), cmap='RdBu_r',
+    im = plot_map(r_e_p,ax=ax1, levels=np.arange(-1,1.01,0.1), cmap='RdBu_r',
                   lw=2,extent=[73, 137, 15, 53])
-    ax1.text(0.5,0.9,'corr(P,E)',transform=ax1.transAxes, fontsize=12,ha='center')
+
+    (p_map.where(p_map>0.05)).plot.contourf(hatches='////', colors='none', add_colorbar=False,ax=ax1)
+
+    ax1.text(0.5,0.9,'$r$(P, E)',transform=ax1.transAxes, fontsize=12,ha='center')
     
     set_lat_lon(ax1, range(80,130,20), range(30,52,10), label=True, pad=0.05, fontsize=10)
     
@@ -40,8 +47,8 @@ def make_plot():
                      ticks=np.arange(-1,1.01,0.2))
     cb.set_label(label='Correlation')
 
-    plt.savefig('../figure/fig_prec_e_corr_map.png',dpi=300,bbox_inches='tight')
+    plt.savefig('../figure/fig_prec_e_corr_map_%s0129.png'%et_data,dpi=300,bbox_inches='tight')
     print('Fig saved')
 
 if __name__=="__main__":
-    make_plot()
+    make_plot(et_data='ERA5')
