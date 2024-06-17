5943b7e82e47fcda56f0d10744146c710d5f3b1e
Yan_dellwork <pkmn99@gmail.com>
2024-06-17 17:54:44 +0800

    fix p_map bug and add dryland internal vs external number

--- code/plot_pe_evi_corr.py
+++ code/plot_pe_evi_corr.py
@@ -39,7 +39,14 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     if grow_season:
         evi = ds_month_range(load_evi_data(temporal_res='mon',source=source))
         dp_ycn=ds_month_range(load_era5_data('prec','mon',cn_label=True))
+       # precipitation contribution from all source grids
         dep_ycn_gleam=ds_month_range(load_e2p_data('e_to_prec_land','mon',end_year=end_year,et_data=et_data))
+
+        # precipitation contribution only from dryland grids
+        dep_ycn_cndry=ds_month_range(load_e2p_data('e_to_prec_cndry','mon',start_year=start_year,end_year=end_year))
+        # precipitation contribution excluding dryland grids
+        dep_ycn_nocndry=dep_ycn_gleam - dep_ycn_cndry
+
 
     else:
         evi = load_evi_data(source=source) # * 10 for terra
@@ -63,7 +70,18 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     # Grid corr between prec con and evi 
     [r_map,p_map]=xr_stats_corr(xr_detrend(evi.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)),
                    xr_detrend(dep_ycn_gleam.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)), dim='time')
-    p_con = p_map<0.05 #<0.1 # significance threshlod to denote regions where evi is sensitive to precipitation
+    p_con = (p_map0<0.05)&(r_map0>0) #<0.1 # significance threshlod to denote regions where evi is sensitive to precipitation
+    
+    # Grid corr between prec con from dryland and evi 
+    [r_map_dry,p_temp]=xr_stats_corr(xr_detrend(evi.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)),
+                   xr_detrend(dep_ycn_cndry.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)), dim='time')
+
+    # Grid corr between prec con from external and evi 
+    [r_map_nodry,p_temp]=xr_stats_corr(xr_detrend(evi.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)),
+                   xr_detrend(dep_ycn_nocndry.where(ai_con).sel(time=slice(str(start_year),str(end_year))).fillna(0)), dim='time')
+    r_map_drydiff=r_map_dry - r_map_nodry
+    r_dry=np.array([(r_map_drydiff>0).sum().values, (r_map_drydiff<0).sum().values])
+    r_dry=r_dry/r_dry.sum()
 
     # Calculate correlation
     # P_E and EVI
@@ -87,7 +105,7 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     ax3in = fig.add_axes([0.375, 0.33, 0.3, 0.125], projection=pr)
 
     # Panel A 
-    ax1.plot(range(start_year,end_year+1),detrend(dep_ycn_gleam.sel(time=slice(str(start_year),str(end_year))).where(ai_con).mean(dim=['lat','lon']).values),'b')
+    ax1.plot(range(start_year,end_year+1),detrend(dep_ycn_gleam.sel(time=slice(str(start_year),str(end_year))).where(ai_con&p_con).mean(dim=['lat','lon']).values),'b')
     
     ax1sec = ax1.twinx()
     ax1sec.plot(range(start_year,end_year+1),
@@ -96,7 +114,7 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     [r,p]=stats_corr(detrend(dep_ycn_gleam.sel(time=slice(str(start_year),str(end_year))).where(ai_con&p_con).mean(dim=['lat','lon'])).values,
                      detrend(evi.sel(time=slice(str(start_year),str(end_year))).where(ai_con&p_con).mean(dim=['lat','lon']).values*10000))
     
-    ax1.text(0.1,0.1,'r=%.2f\np=%.2f'%(r,p),transform=ax1.transAxes)
+    ax1.text(0.05,0.075,'r=%.2f\np=%.3f'%(r,p),transform=ax1.transAxes)
     ax1.set_ylabel('P$_\mathrm{E}$(mm/yr)',color='b',labelpad=0.5)
     ax1.set_xticks(range(2003,2022,3))
     ax1sec.set_ylabel('EVI*10000',color='g',labelpad=1)
@@ -107,7 +125,7 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     sns.barplot(x='Aridity',y='R',data=R_array,hue='var',ax=ax2)
     ax2.set_ylabel('Correlation',labelpad=1)
     ax2.set_xlabel('')
-    ax2.set_ylim([0,1])
+    ax2.set_ylim([-0.20,1])
     ax2.set_xticklabels(['Dry subhumid','Semiarid','Arid','Hyperarid'])
     
     # Add label for p value, double ** for p <0.05 and single * for p< 0.1
@@ -130,7 +148,11 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
 
 #    [print(r_map.where((p_map<0.05)&(ai.Band1==i)).mean()) for i in range(1,5)]
 
-    ax3.text(0.15,0.9,'$r$(P$_\mathrm{E}$, EVI)',transform=ax3.transAxes,fontsize=12,ha='center')
+    ax3.text(0.15,0.95,'$r$(P$_\mathrm{E}$, EVI)',transform=ax3.transAxes,fontsize=12,ha='center')
+    ax3.text(0.075,0.89,'dryland:%d%%'%(np.round(r_dry[0]*100)),
+            transform=ax3.transAxes,fontsize=10,ha='center')
+    ax3.text(0.225,0.89,'external:%d%%'%(np.round(r_dry[1]*100)),
+            transform=ax3.transAxes,fontsize=10,ha='center')
 
     im2 = plot_map(r_map0.where(p_map0<0.05),ax=ax3in, levels=np.arange(-1,1.01,0.1), cmap='RdBu_r',
                   extent=[73, 128, 28, 50])
@@ -149,7 +171,7 @@ def make_plot(et_data='GLEAM',p_data='pe',grow_season=True):
     ax2.text(-0.12, 1.05, 'b', fontsize=14, transform=ax2.transAxes, fontweight='bold')
     ax3.text(-0.05, 1.05, 'c', fontsize=14, transform=ax3.transAxes, fontweight='bold')
     if grow_season:
-        plt.savefig('../figure/fig_pe_evi_corr_%s_gs0129.png'%et_data,dpi=300,bbox_inches='tight')
+        plt.savefig('../figure/fig_pe_evi_corr_%s_gs0617.png'%et_data,dpi=300,bbox_inches='tight')
     else:
         plt.savefig('../figure/fig_pe_evi_corr_%s_annual0129.png'%et_data,dpi=300,bbox_inches='tight')
     print('Fig saved')
